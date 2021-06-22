ff63768ea0f574774ff768b49b5fb61ae3d92bb7
Will-Andrade <willian.santos.andra@outlook.com>
2021-06-21 20:42:17 -0400

    cleans the file

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,7 @@
+import "reflect-metadata";
 import express from "express";
+import "./database";
 
 const app = express();
-
-/**
- * GET => Buscar uma informação
- * POST => Inserir (criar) uma informação
- * PUT => Alterar uma informação
- * DELETE => Remover um dado
- * PATCH => Alterar uma informação específica
-**/
-
-app.get("/test", (request, response) => {
-    return response.send("Olá NLW");
-})
-
-app.post("/test-post", (request, response) => {
-    return response.send("Olá NLW método POST");
-})
 
 app.listen(3000, () => console.log("Server is running"));
