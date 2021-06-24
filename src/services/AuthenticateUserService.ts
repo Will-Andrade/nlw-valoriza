5220535cd34c0ec5ad94ec60e0885c2e280f5036
Will-Andrade <willian.santos.andra@outlook.com>
2021-06-24 08:54:14 -0400

    adds the JWT token generation Service

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,44 @@
+import { getCustomRepository } from "typeorm";
+import { UsersRepositories } from "../repositories/UsersRepositories";
+
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+
+interface IAuthenticateRequest {
+    email: string;
+    password: string;
+}
+
+class AuthenticateUserService {
+    async execute({ email, password }: IAuthenticateRequest) {
+        const usersRepositories = getCustomRepository(UsersRepositories);
+
+        //? Verificar se o email existe
+        const user = await usersRepositories.findOne({
+            email
+        });
+
+        if (!user) {
+            throw new Error("Email/Password incorrect");
+        }
+
+        //? Verificar se a senha está correta
+        const passwordMatch = await compare(password, user.password);
+
+        if (!passwordMatch) {
+            throw new Error("Email/Password incorrect");
+        }
+
+        //? Gerar token
+        const token = sign({
+            email: user.email
+        }, "23797d9a9130322d8435886fe662e8a5", {
+            subject: user.id,
+            expiresIn: "1d"
+        });
+
+        return token;
+    }
+}
+
+export { AuthenticateUserService };
