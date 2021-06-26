b981193236ccdecb00b02b6addd1127d6c7a5082
Will-Andrade <willian.santos.andra@outlook.com>
2021-06-26 18:06:07 -0400

    fixes services getting errors

--- src/controllers/ListUserReceivedComplimentsController.ts
+++ src/controllers/ListUserReceivedComplimentsController.ts
@@ -1,13 +1,12 @@
 import { Request, Response } from "express";
-import { getCustomRepository } from "typeorm";
 import { ListUserReceivedComplimentsService } from "../services/ListUserReceivedComplimentsService";
 
 class ListUserReceivedComplimentsController {
     async handle(request: Request, response: Response) {
         const { user_id } = request;
 
-        const listUserReceivedComplimentsService = 
-            getCustomRepository(ListUserReceivedComplimentsService);
+        const listUserReceivedComplimentsService = new
+            ListUserReceivedComplimentsService();
             
         const compliments = await listUserReceivedComplimentsService
             .execute(user_id);
--- src/controllers/ListUserSentComplimentsController.ts
+++ src/controllers/ListUserSentComplimentsController.ts
@@ -1,13 +1,12 @@
 import { Request, Response } from "express";
-import { getCustomRepository } from "typeorm";
 import { ListUserSentComplimentsService } from "../services/ListUserSentComplimentsService";
 
 class ListUserSentComplimentsController {
     async handle(request: Request, response: Response) {
         const { user_id } = request;
 
-        const listUserSentComplimentsService = 
-            getCustomRepository(ListUserSentComplimentsService);
+        const listUserSentComplimentsService = new
+            ListUserSentComplimentsService();
 
         const compliments = await listUserSentComplimentsService
             .execute(user_id);
