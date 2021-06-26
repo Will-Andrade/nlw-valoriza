7584cd0133dda31f04cd7467903e6883a16886d0
Will-Andrade <willian.santos.andra@outlook.com>
2021-06-26 16:42:53 -0400

    updates how the compliment is received

--- src/controllers/CreateComplimentController.ts
+++ src/controllers/CreateComplimentController.ts
@@ -4,13 +4,14 @@ import { CreateComplimentService } from "../services/CreateComplimentService";
 class CreateComplimentController {
     
     async handle(request: Request, response: Response) {
-        const { tag_id, user_sender, user_receiver, message } = request.body;
+        const { tag_id, user_receiver, message } = request.body;
+        const { user_id } = request;
 
         const createComplimentServer = new CreateComplimentService();
 
         const compliment = await createComplimentServer.execute({
             tag_id,
-            user_sender,
+            user_sender: user_id,
             user_receiver,
             message
         });
